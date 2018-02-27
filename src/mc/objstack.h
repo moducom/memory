e4090e805ba6bc4d0067c0b8ab350fa5ba37a762
Malachi Burke <malachi.burke@gmail.com>
2018-02-26 22:05:28 -0800

    Adding bits more for sanity check/runtime checking

--- src/mc/objstack.h
+++ src/mc/objstack.h
@@ -15,6 +15,9 @@ class ObjStack : public pipeline::MemoryChunk
     typedef pipeline::MemoryChunk memory_chunk_t;
 
     //memory_chunk_t memory_chunk;
+#ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
+    size_t max_length;
+#endif
 
 public:
     struct Descriptor
@@ -24,7 +27,12 @@ public:
 
 public:
     ObjStack(const memory_chunk_t& memory_chunk) :
-        memory_chunk_t(memory_chunk) {}
+        memory_chunk_t(memory_chunk)
+    {
+#ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
+        max_length = memory_chunk.length();
+#endif
+    }
 
     void* alloc(size_t size)
     {
@@ -57,6 +65,18 @@ public:
         size_t sanity_check_size = ((Descriptor*)m_data)->size;
 #endif
     }
+
+
+#ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
+    size_t free() const { return max_length - m_length; }
+#endif
+
+    template <class T>
+    inline void del(T* t)
+    {
+        t->~T();
+        free(sizeof(T));
+    }
 };
 
 
@@ -69,6 +89,8 @@ inline void* operator new(size_t sz, moducom::dynamic::ObjStack& os)
 
 
 #ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
+// placement delete a bit weak, only called during exception unwinding
+// otherwise we have to call the objstack delete helper
 inline void operator delete(void* ptr, moducom::dynamic::ObjStack& os)
 {
     typedef moducom::dynamic::ObjStack::Descriptor descriptor_t;
