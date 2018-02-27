b8230a9774b3cbcea4a0cfb324e66b87fc211190
Malachi Burke <malachi.burke@gmail.com>
2018-02-26 18:20:29 -0800

    Working to make standalone unit tests.  Not working at the moment

---
+++ src/tests/main-test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by malachi on 2/26/18.
+//
+
+#ifdef _MSC_BUILD
+#define CATCH_CONFIG_RUNNER
+#else
+#define CATCH_CONFIG_MAIN
+#endif
+
+#include <catch.hpp>
+
+TEST_CASE("String tests", "[strings]")
+{
+}
