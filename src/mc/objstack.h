a43fb6b55a6a157f13c35ac03d9f0d82326687c0
Malachi Burke <malachi.burke@gmail.com>
2018-02-26 21:48:49 -0800

    Adding nifty placement new/delete helpers for ObjStack

--- src/mc/objstack.h
+++ src/mc/objstack.h
@@ -4,7 +4,7 @@
 
 // Adds extra metadata in objstack allocations for runtime
 // sanity checks
-//#define FEATURE_MC_MEM_OBJSTACK_CHECK
+#define FEATURE_MC_MEM_OBJSTACK_CHECK
 
 namespace moducom { namespace dynamic {
 
@@ -16,6 +16,7 @@ class ObjStack : public pipeline::MemoryChunk
 
     //memory_chunk_t memory_chunk;
 
+public:
     struct Descriptor
     {
         size_t size;
@@ -32,6 +33,7 @@ public:
 #ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
         ((Descriptor*)m_data)->size = size;
         m_data += sizeof(Descriptor);
+        m_length -= sizeof(Descriptor);
 #endif
         void* d = m_data;
 
@@ -51,9 +53,29 @@ public:
 
 #ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
         m_data -= sizeof(Descriptor);
+        m_length += sizeof(Descriptor);
         size_t sanity_check_size = ((Descriptor*)m_data)->size;
 #endif
     }
 };
 
+
 }}
+
+inline void* operator new(size_t sz, moducom::dynamic::ObjStack& os)
+{
+    return os.alloc(sz);
+}
+
+
+#ifdef FEATURE_MC_MEM_OBJSTACK_CHECK
+inline void operator delete(void* ptr, moducom::dynamic::ObjStack& os)
+{
+    typedef moducom::dynamic::ObjStack::Descriptor descriptor_t;
+
+    size_t size = ((descriptor_t*)((uint8_t*)ptr) - sizeof(descriptor_t))->size;
+
+    os.free(size);
+}
+#endif
+
