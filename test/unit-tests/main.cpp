7b5c8d431609bd1cbb06f7a2b5cccc150f1b51ba
Malachi Burke <malachi.burke@gmail.com>
2018-02-27 22:11:03 -0800

    Adding foundation for unit tests

---
+++ test/unit-tests/main.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+
+#ifdef _MSC_BUILD
+#define CATCH_CONFIG_RUNNER
+#else
+#define CATCH_CONFIG_MAIN
+#endif
+
+
+#include "catch.hpp"
+
+TEST_CASE("main", "[main]")
+{
+
+}
+
+/*
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+*/
