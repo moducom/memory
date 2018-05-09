db3efc03a5d7a2f2ca94b81eb59bea5b22a19653
Malachi Burke <malachi.burke@gmail.com>
2018-05-09 13:37:08 -0700

    Prepping for LWIP netbuf helper code

---
+++ src/platform/lwip/lwip-netbuf.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "mc/mem/platform.h"
+
+#ifdef FEATURE_MC_MEM_LWIP
+
+#include <lwip/netbuf.h>
+
+namespace moducom { namespace coap {
+
+class LwipNetbuf
+{
+
+};
+
+}}
+
+#endif
